72119db261ca9c20b4aa59e4ec681dbcdf96442d
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-04-12 11:46:17 +0530

    First commit in Namaste React

---
+++ Class1/App.js
@@ -0,0 +1,28 @@
+const heading = React.createElement(
+  "h1", //Element Type
+  { id: "title" }, //Props Object ==>> Like in html we will give id and class. So here in react we give those props here
+  "Hi, My name is Himanshu Yadav" //Children
+);
+
+const heading2 = React.createElement(
+  "h2", //Element Type
+  { id: "title" }, //Props Object ==>> Like in html we will give id and class. So here in react we give those props here
+  "Hi, My name is Harshita Yadav" //Children
+);
+
+const heading3 = React.createElement(
+  "h3", //Element Type
+  { id: "title" }, //Props Object ==>> Like in html we will give id and class. So here in react we give those props here
+  "Hi, My name is Him Yadav" //Children
+);
+const container = React.createElement("div", { id: "container" }, [
+  heading,
+  heading2,
+  heading3,
+]);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//passing a react element in root
+root.render(container);
+
