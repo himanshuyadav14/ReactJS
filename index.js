c86915e348d7fbcc0efdc0d04a7ca06fe66b1ad1
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-04-13 19:20:34 +0530

    JSX Class commit 1

--- index.js
+++ index.js
@@ -16,34 +16,57 @@ Manages port number also
 Consistent Hashing Algorithms
 Zero Config
 */
-import React from "react";
+
+//npx = npm run
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// const heading = React.createElement(
+//   "h1", //Element Type
+//   { id: "title", key: "h1" }, //Props Object ==>> Like in html we will give id and class. So here in react we give those props here
+//   "Hi, My name is Himanshu Yadav" //Children //Children
+// );
+// console.log(heading);
+
+// //React.createElement gives us object => which then convert into HTML => and puts in DOM
+
+// const heading2 = React.createElement(
+//   "h2", //Element Type
+//   { id: "title", key: "h2" }, //Props Object ==>> Like in html we will give id and class. So here in react we give those props here
+//   "Hi, My name is Harshita Yadav" //Children
+// );
+// console.log(heading2);
+
+// const heading3 = React.createElement(
+//   "h3", //Element Type
+//   { id: "title",key:"h3" }, //Props Object ==>> Like in html we will give id and class. So here in react we give those props here
+//   "Hi, My name is Him Yadav" //Children
+// );
+// console.log(heading3);
+// const container = React.createElement("div", { id: "container" }, [
+//   heading,
+//   heading2,
+//   heading3,
+// ]);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// //   passing a react element in root
+// root.render(container);
+
+import  React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1", //Element Type
-  { id: "title" }, //Props Object ==>> Like in html we will give id and class. So here in react we give those props here
-  "Hi, My name is Himanshu Yadav" //Children //Children
-);
-console.log(heading);
+//JSX?==>   React.createElement()  ==> Object ==> HTML(DOM)
 
-const heading2 = React.createElement(
-  "h2", //Element Type
-  { id: "title" }, //Props Object ==>> Like in html we will give id and class. So here in react we give those props here
-  "Hi, My name is Harshita Yadav" //Children
+// is JSX HTML inside js.....
+const heading = (
+  <h1 id="title" key="h2" classname="">
+    Namaste React
+  </h1>
 );
-console.log(heading2);
 
-const heading3 = React.createElement(
-  "h3", //Element Type
-  { id: "title" }, //Props Object ==>> Like in html we will give id and class. So here in react we give those props here
-  "Hi, My name is Him Yadav" //Children
-);
-console.log(heading3);
-const container = React.createElement("div", { id: "container" }, [
-  heading,
-  heading2,
-  heading3,
-]);
+//Component
+// - Functional Component - NEW
+// - Class Based Component - OLD
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//   passing a react element in root
-root.render(container);
+root.render(heading);
